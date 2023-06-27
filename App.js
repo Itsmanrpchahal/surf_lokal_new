b6382826f1631579fb2b53d9e02feced7e8d513f
rohan <rohanzzz.786@gmail.com>
2023-06-27 09:46:28 +0530

    App

--- App.js
+++ App.js
@@ -42,6 +42,7 @@ const App = () => {
         <Provider store={store}>
           <NavigationContainer>
             <StackNavigator />
+            
           </NavigationContainer>
         </Provider>
       </SafeAreaView>
