70a762e4ebb323ea89b06159c09e76619e0e7ee2
priyanka <priyankasharma6785@gmail.com>
2023-08-08 13:25:11 +0530

    rewards question

--- src/container/MyRewards/Challenges.js
+++ src/container/MyRewards/Challenges.js
@@ -18,14 +18,14 @@ import AsyncStorage from '@react-native-community/async-storage';
 import { getRewardListing } from '../../modules/getRewardListing';
 import { useIsFocused } from '@react-navigation/native';
 import { likeDisLike } from '../../modules/likeDislike';
+import { TypingAnimation } from 'react-native-typing-animation';
 
 const Challenges = () => {
   const [isImageChanged, setIsImageChanged] = useState(false);
   const [isImage, setIsImage] = useState(false);
+  const [isNextText, setNextText] = useState(false);
   const [question, setQuestion] = useState([])
   const [index, setindex] = useState(0)
-  const [postid, setPostid] = useState()
-  const [points, setPoints] = useState()
   const navigation = useNavigation();
   const [user_ID, setUser_ID] = useState();
 
@@ -62,15 +62,15 @@ const Challenges = () => {
           width: '100%',
           marginLeft: 0,
         }}>
-        <Text style={{ fontSize: 18, color: Colors.black, fontFamily: 'Poppins-Medium' }}></Text>
+        <Text style={{ fontSize: 20, color: Colors.black, fontFamily: 'Poppins-Medium', color: Colors.black, textAlign: "center" }}>Challenges</Text>
         <View
           style={{
             flexDirection: 'row',
             width: '90%',
             alignSelf: 'center',
 
-            justifyContent: 'flex-end',
-            alignItems: 'flex-end',
+            justifyContent: 'center',
+            alignItems: 'center',
             overflow: 'visible',
             zIndex: 99,
             position: 'absolute',
@@ -106,78 +106,113 @@ const Challenges = () => {
           </TouchableOpacity>
         </View>
       </View>
+
       <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center', color: Colors.black, height: "100%", paddingBottom: 30 }}>
-        <Text style={{ fontSize: 20, color: Colors.black, fontFamily: 'Poppins-Medium', color: Colors.black }}>Challenges</Text>
-        <Text style={{ fontSize: 16, fontWeight: '500', marginTop: 20, color: Colors.black, fontFamily: 'Poppins-Regular' }}>{"Q."}{index + 1}{" : "}{question[index]?.post_title}</Text>
 
-        <View style={{ flexDirection: "row", justifyContent: 'space-evenly', alignItems: 'center', alignContent: 'center', paddingHorizontal: 130 }} >
-          <TouchableOpacity onPress={() => {
-            setIsImageChanged(true);
-            setIsImage(false)
-            const payload = {
-              user_id: user_ID,
-              title: question[index].post_title,
-              post_id: question[index].ID,
-              points: question[index].points
-            }
-            console.log("payload handleDislie Press", payload)
-            dispatch(likeDisLike(payload)).then(response => {
-              console.log(response)
-            });
-          }
-          }
-            activeOpacity={0.8}
-            style={{
-              width: '100%',
-              alignItems: 'center',
-            }}>
-            <Image
-              source={isImageChanged ? Images.redlike : Images.deletethumb}
-              style={{ height: 50, width: 50, resizeMode: 'contain' }} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => {
-            setIsImage(true)
-            setIsImageChanged(false)
-            const payload = {
-              user_id: user_ID,
-              title: question[index].post_title,
-              post_id: question[index].ID,
-              points: question[index].points
-            }
-            console.log("payload handleFavPress", payload)
-            dispatch(likeDisLike(payload)).then(response => {
-              console.log(response)
-            });
-          }}
-            activeOpacity={0.8}
-            style={{
-              width: '100%',
-              alignItems: 'center',
-            }}>
-            <View style={styles.viewstyle}>
-              <Image
-                source={isImage ? Images.upgreen : Images.upthumb}
-                style={{ height: 50, width: 50, resizeMode: 'contain' }} />
+        {question[index]?.post_title ?
+          <>
+            <View style={{ borderWidth: 1, borderColor: Colors.BorderColor, width: "95%", boxShadow: "0 0 20px 0 rgba(0, 0, 0, 0.2)", padding: 8, height: "40%", borderRadius: 12 }}>
+              <Text style={{ fontSize: 14, marginTop: 20, color: Colors.black, fontFamily: 'Poppins-Regular', width: "97%", height: 60, }}>{"Q."}{index + 1}{" : "}{question[index]?.post_title}</Text>
+              <View style={{ flexDirection: "row", justifyContent: 'center', alignItems: 'center', alignContent: 'center', }} >
+                <TouchableOpacity onPress={() => {
+                  setIsImageChanged(true);
+                  setIsImage(false)
+                  setNextText(true)
 
+                  const payload = {
+                    user_id: user_ID,
+                    title: question[index].post_title,
+                    post_id: question[index].ID,
+                    points: question[index].points
+                  }
+                  console.log("payload handleDislie Press", payload)
+                  dispatch(likeDisLike(payload)).then(response => {
+                    console.log(response)
+                  });
+                }
+                }
+                  activeOpacity={0.8}
+                  style={{
+
+                  }}>
+                  <Image
+                    source={isImageChanged ? Images.redlike : Images.deletethumb}
+                    style={{ height: 50, width: 50, resizeMode: 'contain' }} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {
+                  setIsImage(true)
+                  setIsImageChanged(false)
+                  setNextText(true)
+                  const payload = {
+                    user_id: user_ID,
+                    title: question[index].post_title,
+                    post_id: question[index].ID,
+                    points: question[index].points
+                  }
+                  console.log("payload handleFavPress", payload)
+                  dispatch(likeDisLike(payload)).then(response => {
+                    console.log(response)
+                  });
+                }}
+                  activeOpacity={0.8}
+                  style={{
+                    // width: '100%',
+                    // alignItems: 'center',
+                  }}>
+                  <View style={styles.viewstyle}>
+                    <Image
+                      source={isImage ? Images.upgreen : Images.upthumb}
+                      style={{ height: 50, width: 50, resizeMode: 'contain' }} />
+
+                  </View>
+                </TouchableOpacity>
+              </View>
+              <View style={{ flexDirection: "row", justifyContent: "space-between", width: "100%", position: "relative" }}>
+
+                {index <= 0 ? "" : <>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setindex(index - 1)
+                    }}
+                    style={[styles.rew, { position: "absolute", left: 0 }]}
+                  ><Text style={[styles.text, { color: Colors.primaryBlue, fontFamily: 'Poppins-Regular', fontSize: 14 }]}>Back</Text>
+                  </TouchableOpacity>
+                </>}
+
+
+                <TouchableOpacity
+                  onPress={() => {
+                    setindex(index + 1)
+                    setIsImage(false)
+                    setIsImageChanged(false);
+                    setNextText(false)
+                  }} style={[styles.rew, { position: "absolute", right: 0 }]}>
+                  <Text style={[styles.text, { color: Colors.primaryBlue, fontFamily: 'Poppins-Regular', fontSize: 14 }]}>{isNextText ? "Next" : "Skip"}</Text>
+                </TouchableOpacity>
+              </View>
             </View>
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity
-          onPress={() => {
-            setindex(index + 1)
-            setIsImage(false)
-            setIsImageChanged(false);
-          }} style={[styles.rew]}>
-          <Text style={[styles.text, { color: Colors.white, fontFamily: 'Poppins-Regular', fontSize: 14 }]}> Skip </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            setindex(index - 1)
-          }}
-          style={[styles.rew,]}
-        >
-          <Text style={[styles.text, { color: Colors.white, fontFamily: 'Poppins-Regular', fontSize: 14 }]}>Back </Text>
-        </TouchableOpacity>
+          </>
+
+          : <>
+            <Text style={{ fontSize: 16, fontWeight: '500', marginTop: 20, color: Colors.black, fontFamily: 'Poppins-Regular' }}>Wait for your challenges</Text>
+            <TypingAnimation
+              dotColor="black"
+              dotMargin={10}
+              dotAmplitude={2}
+              dotSpeed={0.15}
+              dotRadius={1}
+              dotX={8}
+              dotY={5}
+              style={{ marginTop: 25, marginLeft: -3 }}
+            />
+          </>
+        }
+
+
+
+
+
+
       </View>
     </SafeAreaView>
   )
@@ -205,7 +240,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: Colors.black,
     marginLeft: 8,
-    fontFamily: 'Poppins-Regular'
+    fontFamily: 'Poppins-Regular',
   },
   slideOuter: {
     width: "100%",
@@ -215,16 +250,6 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     borderRadius: 18,
   },
-  // slide: {
-  //   width: screenWidth - 40,
-  //   height: screenHeight / 3,
-  //   borderRadius: 18,
-  //   margin: 20,
-  //   justifyContent: 'space-between',
-  //   alignItems: 'center',
-  //   resizeMode: 'contain',
-  //   flexDirection: 'row',
-  // },
   title: {
     fontSize: 32,
     fontWeight: 'bold',
@@ -262,16 +287,17 @@ const styles = StyleSheet.create({
   },
   rew: {
     height: 45,
-    // width: 130,
+    width: 130,
     borderRadius: 17,
     borderWidth: 1,
     paddingHorizontal: 10,
     marginTop: 18,
     // marginRight: '10%',
-    backgroundColor: Colors.primaryBlue,
+    // backgroundColor: Colors.primaryBlue,
     justifyContent: 'center',
     alignItems: 'center',
-    marginHorizontal: 8
+    marginHorizontal: 8,
+    borderColor: Colors.primaryBlue
   },
 });
 export default Challenges
