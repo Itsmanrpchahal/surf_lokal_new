1ee15ac82f785147dae6964f682fada9d4a5b269
Anshu <noobhack91@gmail.com>
2023-09-01 17:04:22 +0530

    homepagerecycylebin

--- src/container/RecycleBin/RecycleBin.js
+++ src/container/RecycleBin/RecycleBin.js
@@ -246,26 +246,9 @@ const RecycleBin = () => {
          style={styles.slide} />
       </TouchableOpacity>
       <View
-        style={{
-          backgroundColor: Colors.surfblur,
-          position: 'absolute',
-          top: 8,
-          left: 16,
-          borderRadius: 5,
-          justifyContent: 'center',
-          alignItems: 'center',
-          paddingHorizontal: 8,
-          paddingVertical: 4,
-        }}>
+        style={styles.listingkeycover}>
         <Text
-          style={{
-            fontSize: DeviceInfo.getDeviceType() === 'Tablet'?24:12,
-            color: Colors.white,
-            fontFamily: 'Poppins-Regular',
-            marginBottom: 0,
-            lineHeight: DeviceInfo.getDeviceType() === 'Tablet'?28:14,
-            paddingTop: 4,
-          }}>
+          style={styles.innerlistingkey}>
        RX-{item.ListingKey}
         </Text>
       </View>
@@ -1513,6 +1496,25 @@ height:50
 };
 
 const styles = StyleSheet.create({
+  innerlistingkey:{
+    fontSize: DeviceInfo.getDeviceType() === 'Tablet'?24:12,
+    color: Colors.white,
+    fontFamily: 'Poppins-Regular',
+    marginBottom: 0,
+    lineHeight: DeviceInfo.getDeviceType() === 'Tablet'?28:14,
+    paddingTop: 4,
+  },
+  listingkeycover:{
+    backgroundColor: Colors.surfblur,
+    position: 'absolute',
+    top: 8,
+    left: 16,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
   container: {
     flex: 1,
     backgroundColor: Colors.white,
